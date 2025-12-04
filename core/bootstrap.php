7ed0ac7fdc9412995c898322afbd76d9653fd32f
Hang A Tung <toojhaam123@gmail.com>
2025-12-04 22:13:42 +0700

    MyChange

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,18 +1,18 @@
 <?php
 // Load các controllers cần thiết
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_User.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_Introduction.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_Artifact.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_News.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_Exhibition.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_Comment.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/C_Permission.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_User.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_Introduction.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_Artifact.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_News.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_Exhibition.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_Comment.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/app/controllers/C_Permission.php';
 
 // Route cho từng module 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_User.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_Introduction.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_Artifact.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_News.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_Exhibition.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_Comment.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/routes/R_Permission.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_User.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_Introduction.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_Artifact.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_News.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_Exhibition.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_Comment.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/BaoTangChienThangDienBienPhu/routes/R_Permission.php';
